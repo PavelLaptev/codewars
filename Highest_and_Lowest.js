05f618578af2fcf7f45753597fe64105c902c542
Pavel <graphics.dario@gmail.com>
2017-01-10 20:16:46 +0300

    Update Highest_and_Lowest.js

--- Highest_and_Lowest.js
+++ Highest_and_Lowest.js
@@ -5,9 +5,6 @@ function highAndLow(numbers){
   let result = [];
   for (i=0; i<strLength; i++){
     numArr.push(Number(strArr[i]));
-    // for (j=0; j<numArr.length; j++){
-    //   console.log(numArr);
-    // }
   }
   result.push(Math.max.apply(Math, numArr));
   result.push(Math.min.apply(Math, numArr));
